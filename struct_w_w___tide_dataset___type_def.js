ed0777b1e962abd5054b0790454546e1f7d86bfa
HarveyBates <harvey_bates@hotmail.com>
2022-06-14 03:54:03 +0000

    Deploying to gh-pages from @ DPIclimate/ha-closure-analysis@d41734fb7962fb510a9774f826c108e0c4ee1be5 🚀

--- struct_w_w___tide_dataset___type_def.js
+++ struct_w_w___tide_dataset___type_def.js
@@ -5,5 +5,8 @@ var struct_w_w___tide_dataset___type_def =
     [ "high_tide_timestamps", "struct_w_w___tide_dataset___type_def.html#a1c1b4657063b2c15f8e6bbeca9c6af56", null ],
     [ "high_tide_values", "struct_w_w___tide_dataset___type_def.html#aff29855554ae8ff6256e18d46c2cfe28", null ],
     [ "low_tide_timestamps", "struct_w_w___tide_dataset___type_def.html#a4923265fc51b8eae4a941a9d932d2495", null ],
-    [ "low_tide_values", "struct_w_w___tide_dataset___type_def.html#a0ea68961c37618bde2a8cdd37c3a46a0", null ]
+    [ "low_tide_values", "struct_w_w___tide_dataset___type_def.html#a0ea68961c37618bde2a8cdd37c3a46a0", null ],
+    [ "n_days", "struct_w_w___tide_dataset___type_def.html#aa42c177abbbabc84922ff275be0efc09", null ],
+    [ "n_high_tide", "struct_w_w___tide_dataset___type_def.html#a76a81033d6c5af3e8d7228c2b2b8157c", null ],
+    [ "n_low_tide", "struct_w_w___tide_dataset___type_def.html#a363dabc9c828c896d7537e54ff54b8ee", null ]
 ];
