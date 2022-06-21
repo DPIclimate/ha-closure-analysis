2c0e090c79f2d6428f51985d3130aee021f4ea0b
Harvey Bates <phenobottle@gmail.com>
2022-06-21 14:02:29 +1000

    added method to parse all NSW oyster harvesting area statuses

---
+++ include/FoodAuthority/harvest_areas.h
@@ -0,0 +1,8 @@
+//
+// Created by Harvey Bates on 20/6/22.
+//
+
+#ifndef PROGRAM_HARVEST_AREAS_H
+#define PROGRAM_HARVEST_AREAS_H
+
+#endif //PROGRAM_HARVEST_AREAS_H
---
+++ src/FoodAuthority/harvest_areas.c
@@ -0,0 +1,5 @@
+//
+// Created by Harvey Bates on 20/6/22.
+//
+
+#include "harvest_areas.h"
